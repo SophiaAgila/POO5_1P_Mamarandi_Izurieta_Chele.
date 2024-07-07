8f9314715cf9c82fe51635a96422febbab8604f2
KevinIzu <kevdaizu@espol.edu.ec>
2024-07-07 18:34:48 -0500

    Agregue Archivos.java

---
+++ util/Archivos.java
@@ -0,0 +1,13 @@
+package main.java.com.revista.util;
+
+/**
+ * Define las rutas de los archivos de texto
+ */
+public abstract class Archivos {
+    public final static String USUARIOS = "resources/usuarios.txt";
+    public final static String EDITORES = "resources/editores.txt";
+    public final static String REVISORES = "resources/revisores.txt";
+    public final static String AUTORES = "resources/investigadores.txt";
+    public final static String ARTICULOS = "resources/articulos.txt";
+    public final static String REVISIONES = "resources/revisiones.txt";
+}
